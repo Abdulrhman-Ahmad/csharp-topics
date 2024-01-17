747987143bf826da1fbb4a2e48fa89a23182df56
Abdularhman Ahmed <abdwbedoo@gmail.com>
2024-01-17 19:24:38 +0200

    Update User_Secrets

--- User_Secrets/Program.cs
+++ User_Secrets/Program.cs
@@ -1,20 +1,33 @@
 ﻿/*
- * - What is the purpose of using user-secrets
- *          - is to kep sensitive informatino, suah as API keys, connection strings, or other private configuration settings
- *          - Seperate from your source code and project files 
- *          - this is particularly important when workingon a team or sharing code in a version-controlled environment
+ *  Purpose of Using
+ *          - Keep sensitive information, suah as API keys, connection strings, or other private configuration settings
+ *          - These secrets are separated from your source code and project files (not pushable or movable)
+ *          - This is very important when Working with a team or sharing code in a version-controlled environment
  * 
- * Install these two packages to use configuration also to access secrets list 
+ *  Install these two packages to use configuration also to access secrets list 
  *          - Include="Microsoft.Extensions.Configuration" Version="5
  *          - Include="Microsoft.Extensions.Configuration.UserSecrets 
  *          
- * - To Start
- *      - Set Secret
- *          - dotnet user-secrets set "
- *   [ init, set, clear, list, remove ] 
- * - dotnet user-secrets init                               // will generate UserSecretsId => ff538252-b285-47a0-a006-466ee4e16984
- * - dotnet user-secrets set "SecretName" "SecretValue"     // will save the secret with the specified name in the project file
- * - dotnet user-secrets list                               // Show all the secrets
+ *  Note: 
+ *      - If we pushed the project the secrets won't be pushed
+ *      - If we pulled the pushed project Only the initialization of the user-secrets will remain but without any secrets
+ *      - On initialization of the project, it generates UserSecretsId and saved to the project file
+ *          
+ *  To Start
+ *      - Init User Secrets
+ *          - dotnet user-secrets init
+ *      
+ *      - Set a Secret
+ *          - dotnet user-secrets set "SecretName" "SecretValue"
+ *          
+ *      - Show All Secrets
+ *          - dotnet user-secrets list 
+ *          
+ *      - Clear All Secrets
+ *          - dotnet user-secrets clear
+ *          
+ *      - Delete specified secret
+ *          - dotnet user-secrets remove "SecretName"
  */
 using Microsoft.Extensions.Configuration;
 
